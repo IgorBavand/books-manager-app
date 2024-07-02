e909e519f923b1903f37a2248a874e7b7051ff71
Guerreiro <igor.bvn@gmail.com>
2024-07-02 18:23:48 -0300

    fix: toolbar login page

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 
@@ -7,11 +7,19 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
   constructor(
     private authService: AuthenticationService,
     private router: Router
   ) {}
+
+  isLogged: boolean = false;
+
+  ngOnInit(): void {
+    this.authService.isLogged().subscribe((isLogged) => {
+      this.isLogged = isLogged;
+    });
+  }
 
   logout() {
     this.authService.logout();
